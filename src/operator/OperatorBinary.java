07e2e371dd128165acdd2ff7fe9527ecf832e3f1
Muhamad Fiqri Perdana <mfperdana13@gmail.com>
2020-05-03 08:07:10 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,5 +4,13 @@ public class OperatorBinary {
     String nama = "Muhammad Fiqri Perdana";
     int jumlahSks = 24;
     double indexsPrestasiKumulatif  =  3.89;
-    
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
